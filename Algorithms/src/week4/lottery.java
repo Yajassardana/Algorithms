687b08ca7c961594d262ea54cfce86ecd84f19a7
Yajas Sardana <yajassardana@gmail.com>
2020-06-25 07:05:16 +0530

    DP-1

---
+++ Algorithms/src/week4/lottery.java
@@ -0,0 +1,10 @@
+package week4;
+
+public class lottery {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Algorithms/src/week5/moneyChange.java
@@ -0,0 +1,19 @@
+package week5;
+
+import java.util.Scanner;
+
+public class moneyChange {
+
+    private static int getChange(int m) {
+        //write your code here
+        return m / 4;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        System.out.println(getChange(m));
+
+    }
+
+}
