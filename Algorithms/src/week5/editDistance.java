2936a22d838002a71b8251716b916fb4b3e006a9
Yajas Sardana <yajassardana@gmail.com>
2020-06-26 00:15:49 +0530

    naive edit distance

---
+++ Algorithms/src/week5/editDistance.java
@@ -0,0 +1,17 @@
+package week5;
+
+public class editDistance {
+	  public static int EditDistance(String s, String t) {
+		    //write your code here
+		    return 0;
+		  }
+		  public static void main(String args[]) {
+		    Scanner scan = new Scanner(System.in);
+
+		    String s = scan.next();
+		    String t = scan.next();
+
+		    System.out.println(EditDistance(s, t));
+		  }
+
+}
