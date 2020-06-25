020cc0046e3dc39b9596b8d36fd92bb622d63897
Yajas Sardana <yajassardana@gmail.com>
2020-06-25 07:19:45 +0530

    money change again

--- Algorithms/src/week4/lottery.java
+++ Algorithms/src/week4/lottery.java
@@ -1,10 +1,62 @@
 package week4;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class lottery {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int occurences(int start,int end,int[] points) {
+		int mid = 
+		while()
+		int count =  0;
+		return count;
 	}
+
+	  private static int[] fastCountSegments(int[] starts, int[] ends,int l,int r, int[] points) {
+	        int[] cnt = new int[points.length]; 
+	        Arrays.sort(points);
+	        for (int i = 0; i < points.length; i++) {
+	            for (int j = 0; j < starts.length; j++) {
+	                if (starts[j] <= points[i] && points[i] <= ends[j]) {
+	                    cnt[i]++;
+	                }
+	            }
+	        }
+	        return cnt;
+	    }
+
+	    public static int[] naiveCountSegments(int[] starts, int[] ends, int[] points) {
+	        int[] cnt = new int[points.length];
+	        for (int i = 0; i < points.length; i++) {
+	            for (int j = 0; j < starts.length; j++) {
+	                if (starts[j] <= points[i] && points[i] <= ends[j]) {
+	                    cnt[i]++;
+	                }
+	            }
+	        }
+	        return cnt;
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        int n, m;
+	        n = scanner.nextInt();
+	        m = scanner.nextInt();
+	        int[] starts = new int[n];
+	        int[] ends = new int[n];
+	        int[] points = new int[m];
+	        for (int i = 0; i < n; i++) {
+	            starts[i] = scanner.nextInt();
+	            ends[i] = scanner.nextInt();
+	        }
+	        for (int i = 0; i < m; i++) {
+	            points[i] = scanner.nextInt();
+	        }
+	        //use fastCountSegments
+	        int [] cnt = fastCountSegments(starts,ends,0,n-1,points);
+	        for (int x : cnt) {
+	            System.out.print(x + " ");
+	        }
+	    }
+
 
 }
--- Algorithms/src/week5/moneyChange.java
+++ Algorithms/src/week5/moneyChange.java
@@ -5,8 +5,21 @@ import java.util.Scanner;
 public class moneyChange {
 
     private static int getChange(int m) {
-        //write your code here
-        return m / 4;
+        int minMoney[] = new int [m+1]; 
+        minMoney[0]=0;
+        int coins [] = {1,3,4};
+        for(int i=1;i<=m;i++) {
+        	minMoney[i]= Integer.MAX_VALUE;
+        	for(int j:coins) {
+        		if(i>=j) {
+        			int minMon = minMoney[i-j]+1;
+        			if(minMon<minMoney[i]) {
+        				minMoney[i]=minMon;
+        			}
+        		}
+        	}
+        }
+        return minMoney[m];
     }
 
     public static void main(String[] args) {
