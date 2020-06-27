01923442144444d98ff61bbfe8aaa719055c35ec
Yajas Sardana <yajassardana@gmail.com>
2020-06-28 03:06:49 +0530

    longest subsequence

--- Algorithms/src/week5/LongestCommonSubSequence.java
+++ Algorithms/src/week5/LongestCommonSubSequence.java
@@ -5,9 +5,28 @@ import java.util.Scanner;
 public class LongestCommonSubSequence {
 
 
-    private static int lcs2(int[] a, int[] b) {
+    private static int lcs2(int[] s, int[] t) {
         //Write your code here
-        return Math.min(a.length, b.length);
+        
+        int [][]dpTable = new int [s.length+1][t.length+1];
+	    int match=0,misMatch=0,ins=0,del=0;
+	    for(int i = 1;i<=s.length;i++) {
+	    	for(int j=1;j<=t.length;j++) {
+	    		ins = dpTable[i][j-1];
+	    		del = dpTable[i-1][j];
+	    		match = dpTable[i-1][j-1]+1;
+	    		misMatch = dpTable[i-1][j-1];
+	    		if(s[i-1]==t[j-1]) {
+	    			dpTable[i][j]=Math.max(ins, Math.max(match, del));
+	    		}
+	    		else {
+	    			dpTable[i][j] = Math.max(ins,Math.max(misMatch, del));
+	    		}
+	    	}
+	    }
+	    return dpTable[s.length][t.length];
+        
+        
     }
 
     public static void main(String[] args) {
